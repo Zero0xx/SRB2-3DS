b5604a19ccf19bfbf65ee661fb2d39535f76f17c
Joe <joseph14078@gmail.com>
2021-05-07 16:42:47 -0400

    Seriously?

--- downloadassets.sh
+++ downloadassets.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-INSTALLERURL="https://github.com/STJr/SRB2/releases/download/SRB2_release_2.2.9/SRB2-v229-Installer.exe"
+FULLURL="https://github.com/STJr/SRB2/releases/download/SRB2_release_2.2.9/SRB2-v229-Full.zip"
 PATCHURL="https://github.com/STJr/SRB2/releases/download/SRB2_release_2.2.9/SRB2-v229-Patch.zip"
 DOWNLOADCMD="curl -Lo"
 
 cd assets
 rm *.srb *.pk3 *.dta *.wad
-${DOWNLOADCMD} srb2installer.exe ${INSTALLERURL}
-7z e -y -aoa srb2installer.exe "*.srb" "*.pk3" "*.dta" "*.wad"
-rm srb2installer.exe
+${DOWNLOADCMD} srb2full.zip ${FULLURL}
+unzip -o -j srb2full.zip "*.srb" "*.pk3" "*.dta" "*.wad" "*.dat"
+rm srb2full.zip
 
 cd patch
 rm *.srb *.pk3 *.dta *.wad
 if [ ! -z "$PATCHURL" ];
 then
     ${DOWNLOADCMD} srb2patch.zip ${PATCHURL}
-    unzip -o -j srb2patch.zip "*.srb" "*.pk3" "*.dta" "*.wad"
+    unzip -o -j srb2patch.zip "*.srb" "*.pk3" "*.dta" "*.wad" "*.dat"
     rm srb2patch.zip
     cp * ..
 fi
